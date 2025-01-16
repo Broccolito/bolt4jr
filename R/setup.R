253e8786877dcc4c8c1c51e4a3004082cd1bd21c
Broccolito <wanjungu001@gmail.com>
2025-01-16 15:23:49 -0800

    auto commit

--- R/setup.R
+++ R/setup.R
@@ -1,21 +1,27 @@
 .onLoad = function(libname, pkgname) {
-  if(!requireNamespace("reticulate", quietly = TRUE)){
+  if (!requireNamespace("reticulate", quietly = TRUE)) {
     stop("The 'reticulate' package is required but not installed.", call. = FALSE)
   }
 
   envname = "bolt4jr"
-  conda_path = reticulate::conda_binary("auto")
 
-  if(is.null(conda_path)){
-    warning("No conda binary found. Please install Miniconda or Anaconda.", call. = FALSE)
-    return(invisible(NULL))
+  conda_path = tryCatch({
+    reticulate::conda_binary("auto")
+  }, error = function(e) NULL)
+
+  if (is.null(conda_path)) {
+    message("No Conda binary found. Installing Miniconda...")
+    reticulate::install_miniconda()
+    conda_path = reticulate::conda_binary("auto")
   }
 
   envs = reticulate::conda_list()
   env_exists = any(envs$name == envname)
 
-  if(!env_exists){
+  if (!env_exists) {
+    message(paste("Creating Conda environment:", envname))
     reticulate::conda_create(envname = envname, packages = c("python=3.9"))
+    message(paste("Installing 'neo4j' in environment:", envname))
     reticulate::conda_install(envname = envname, packages = "neo4j", pip = TRUE)
   }
 
