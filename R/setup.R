5a4e1d750bd28d57c0fca184c07c0061ce8d0a73
Broccolito <wanjungu001@gmail.com>
2025-01-17 10:29:49 -0800

    auto commit

--- R/setup.R
+++ R/setup.R
@@ -1,3 +1,4 @@
 .onLoad = function(libname, pkgname){
-  message("bolt4jr has been loaded. \nIf this is your first time using the package, required Python dependencies will be installed.")
+  packageStartupMessage("bolt4jr has been loaded.")
+  packageStartupMessage("If this is your first time using the package, required Python dependencies will be installed.")
 }
